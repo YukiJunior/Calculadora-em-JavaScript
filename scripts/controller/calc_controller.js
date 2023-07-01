ba2bf3cacbcc700f73e7380355c656e7fbc97a51
Yuki Junior <yukijunioruesugi@gmail.com>
2023-07-01 12:35:15 -0300

    Readme

--- scripts/controller/calc_controller.js
+++ scripts/controller/calc_controller.js
@@ -2,6 +2,7 @@ class CalcController {
 
     constructor(){
         // _ antes da variável informa que ela privado
+        this._operation = [];
         this._locale = 'pt-BR';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -30,6 +31,7 @@ class CalcController {
         }, 10000);*/
 
     }
+
     addEventListenerAll(element, events, fn){
 
         events.split(' ').forEach(event => {
@@ -37,7 +39,120 @@ class CalcController {
             element.addEventListener(event, fn, false);
 
         });
+    }
 
+    clearAll(){
+
+        this._operation = []
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+
+        
+    }
+
+    getLastOperation(value){
+
+        return this._operation[this._operation.length -1];
+
+    }
+
+    setLastOperation(value){
+
+        this._operation[this._operation.length -1] = value;
+
+    }
+
+    isOperator(value){
+        
+        return (['+', '-', '*', '%', '/'].indexOf(value)> -1)
+              
+    }
+
+    addOperation(value){
+
+        console.log('a',this.getLastOperation())
+
+        if(isNaN(this.getLastOperation())){
+            // recebe String ou operador
+            if (this.isOperator(value)){
+                // troca de operador
+                this.setLastOperation(value);            }
+            else if(isNaN(value)){ 
+                
+                console.log(value);
+            }else{
+                this._operation.push(value);
+            }
+        }
+        else {
+            
+            let newValue = this.getLastOperation().toString() + value.toString();
+            this.setLastOperation(parseInt(newValue));
+        }
+        
+        console.log(this._operation);
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+
+    }
+
+    execBtn(value){
+
+        switch (value) {
+            case 'ac':
+
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                this.addOperation('+');
+                break;
+            case 'subtracao':
+                this.addOperation('-');
+                break;
+            case 'multiplicacao':
+                this.addOperation('*');
+                break;
+            case 'divisao':
+                this.addOperation('/');
+                break;
+            case 'porcento':
+                this.addOperation('%');
+                break;
+            case 'igual':
+                
+                break;
+            case 'ponto': 
+            this.addOperation('.');
+                break;
+                
+            case "0": 
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+        }
 
     }
 
@@ -47,13 +162,15 @@ class CalcController {
             
         buttons.forEach((btn, index)=>{
 
-            this.addEventListenerAll(btn,"click drag mouseover", e => {
+            this.addEventListenerAll(btn,"click drag", e => {
 
-                console.log(btn.className.baseVal.replace("btn-", ""));
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e =>{
-                btn.
+                btn.style.cursor = "pointer";
 
             });
         });
